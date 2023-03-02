1a5ee6b1fe43a2dc3d0e94423b9f1b9945f9122e
Iyanuoluwa Odusanya <iyanuoluwaodusanya@Iyanuoluwas-MacBook-Pro.local>
2023-03-02 20:00:34 +0100

    set up navbar and color theme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import { Outlet } from "react-router-dom";
-import React from "react";
+// import { Outlet } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import NavBar from "./components/organisms/NavBar";
+import NavBarContext from "./contexts/NavBarContext";
 
 function App() {
+  const [darkTheme, setDarkTheme] = useState(true);
+
+  // nav bar data
+  const navBarObject = {
+    darkTheme: darkTheme,
+    setDarkTheme: setDarkTheme,
+  };
+
+  // dark theme swicther
+  useEffect(() => {
+    if (darkTheme) document.documentElement.classList.add("dark");
+    else document.documentElement.classList.remove("dark");
+  }, [darkTheme]);
 
   return (
-    <div>
-        APP
-    </div>
+    <main
+      className="min-h-screen text-sm bg-VeryLightGray dark:bg-VeryDarkBlue text-VeryDarkBlue 
+    dark:text-White sm:text-base"
+    >
+      <NavBarContext.Provider value={navBarObject}>
+        <NavBar />
+      </NavBarContext.Provider>
+    </main>
   );
 }
 
