6dfdd3489ee8c411ae5d892a11a3b80494b43a7e
Iyanuoluwa Odusanya <iyanuoluwaodusanya@Iyanuoluwas-MacBook-Pro.local>
2023-03-03 07:13:11 +0100

    test dropDown

--- src/test/components/organisms/dropDown.spec.js
+++ src/test/components/organisms/dropDown.spec.js
@@ -1,27 +1,19 @@
-// import React from "react";
-// import { shallow } from 'enzyme'
-// import DropDown from "../../../components/organisms/dropDown";
+import React from "react";
+import { shallow } from 'enzyme'
+import DropDown from "../../../components/organisms/dropDown";
 
-// jest.mock('react-router-dom', () => ({
-//     ...jest.requireActual('react-router-dom'),
-//     useSearchParams: () => [],
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useSearchParams: () => [],
 
-// }))
-
-// jest.mock('react', () => ({
-//     ...jest.requireActual('react'),
-//     useContext: () => ({
-//         // regionIndex:1,
-//         // searchFilter: ''
-//     }),
-//     useState: () => [],
-
-// }))
+}))
 
 
-// describe('test mainPage', () => {
-//     it('test mainPage', () => {
-//         const btn = shallow(<DropDown />)
-//         expect(btn).toMatchSnapshot();
-//     })
-// })
+
+
+describe('test mainPage', () => {
+    it('test mainPage', () => {
+        const btn = shallow(<DropDown />)
+        expect(btn).toMatchSnapshot();
+    })
+})
